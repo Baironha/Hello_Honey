40d6459213098cc37ff3e452ff06417d6260cb2c
HUNTER4FWD <jgarcia@fwdcostarica.com>
2025-07-02 00:23:35 -0600

    actualizacion 2

--- Front-End_Honey/src/pages/Economia_page.jsx
+++ Front-End_Honey/src/pages/Economia_page.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import SidebarEconomia from '../components/SidebarEconomia'
 import Navbar_Honey from '../components/Navbar_Honey'
 import FooterHonetComponent from '../components/FooterHoneyComponent'
+import'../style/EconomiaPage.css'
 function Economia_page() {
   return (
     <div>
         <Navbar_Honey/>
         <SidebarEconomia/>
+       <div className='FooterECopage'>
         <FooterHonetComponent/>
+       </div>
+        
     </div>
   )
 }
