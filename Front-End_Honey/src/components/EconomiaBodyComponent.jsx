16ffdb7d8c2c3d716f796152735a8372ca4cccad
HUNTER4FWD <jgarcia@fwdcostarica.com>
2025-07-03 01:59:31 -0600

    avance para vercel5

--- Front-End_Honey/src/components/EconomiaBodyComponent.jsx
+++ Front-End_Honey/src/components/EconomiaBodyComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
+/* NO ES ERROR, SOLO ME ABVIERTE DE EL USO DE LIRERIA */
 import "../style/EconomiaBodyStyle.css";
 
 function EconomiaBodyComponent() {
